0d9469c6dc25b557915f184f9b5382be675d56d8
rolledback <matthew.rayermann@gmail.com>
2013-10-28 13:42:19 -0500

    fixed warning when using strict, did some other stuff

--- final.pl
+++ final.pl
@@ -1,61 +1,62 @@
 #!/usr/bin/env perl
+use strict;
 
-use Data::Dumper;
-use POSIX 'ceil';
 $| = 1;
-sub percentagePrint {
-   $time = shift;
-   $percent = shift;
-   $truePercent = $percent;
-   if($percent > 50 && $percent < 80) { $percent = 100 - $percent; }
-   print "\r[";
-   for $i (0..(($percent / 2) - 1)) { print "*"; }
-   for $i (($percent / 2)..49) { print "-"; }
-   print "]";
-   if($truePercent > 70 && $truePercent < 80) { print " Just kidding!"; }   
-   else { print " $percent% ($time seconds)   "; }
-}
 
 my %actors;
-my $movies;
+my %movies;
 my $totalTime;
 my $actor;
+my $line = 0;
+my $oldPercent = -1;
+my $curPercent = 0;
 
+my @lines;
+foreach my $arg (@ARGV) {
+   push(@lines, `zcat $arg`);
+}
 
-foreach $arg (@ARGV) {
-   print "Opening $arg...\n";
-   my @lines = `zcat $arg`;
-   my $startTime = time();
-   print "Done.\nParsing $arg...\n";
-   $totalLines = scalar @lines;
-   my $line = 0;
-   foreach (@lines) {
-      if(++$line % 10000 == 0) {
-         #animated progress bar adds overhead to parsing, comment out for much faster times!
-         percentagePrint(time() - $startTime, ceil($line / $totalLines * 100));
+print "Parsing files...\n";
+my $startTime = time();
+my $totalLines = scalar @lines;
+
+foreach (@lines) {
+   $line++;
+   $curPercent = int($line / $totalLines * 100);
+   if($curPercent != $oldPercent) {
+      $oldPercent = $curPercent;
+      $totalTime = (time() - $startTime);
+      percentagePrint($totalTime, $curPercent);
+   }
+   chomp();
+   if($_ =~ /^(.*?)\t+(.+?\([\d]+(\/[IXV]*)*\))(.*)/) {  
+      (my $gActor, my $gMovie, my $gRoman, my $gExtra) = ($1, $2, $3, $4);
+      if($gActor !~ /^\s*$/) {
+         $actor = $gActor;      
       }
-      chomp();
-      if($_ =~ /^(.*?)\t+(.+?\([\d]+(\/[IXV]*)*\))(.*)/) {  
-         (my $gActor, my $gMovie, my $gRoman, my $gExtra) = ($1, $2, $3, $4);
-         if($gActor !~ /^\s*$/) {
-            $actor = $gActor;      
-         }
-         if(index($gExtra, "(TV)") == -1 && index($gExtra, "(VG)") == -1 && index($gExtra, "(V)") == -1 && index($gExtra, "(archive footage)") == -1) {
-            if(index($gMovie, "\"") != 0) {
-               $actors{$actor}{$gMovie} = 0;
-               $movies{$gMovie}{$actor} = 0;
-            }
+      if(index($gExtra, "(TV)") == -1 && index($gExtra, "(VG)") == -1 && index($gExtra, "(V)") == -1 && index($gExtra, "(archive footage)") == -1) {
+         if(index($gMovie, "\"") != 0) {
+            $actors{$actor}{$gMovie} = 0;
+            $movies{$gMovie}{$actor} = 0;
          }
       }
    }
-   $totalTime += (time() - $startTime);
-   print "\nDone.\n";
 }
 
-$numActors = keys %actors; 
-$numMovies = keys %movies;
-$rate = ceil($numActors / $totalTime);
-print "\nA total of $numActors actors in $numMovies movies parsed in $totalTime seconds, at a rate of $rate actors/second.\n";
+my $numActors = keys %actors; 
+my $numMovies = keys %movies;
+my $rate = int($numActors / $totalTime);
+print "\nDone, total of $numActors actors in $numMovies movies parsed in $totalTime seconds, at a rate of $rate actors/second.\n";
 
-#print Dumper \%actors;
-#print Dumper \%movies;
+sub percentagePrint {
+   my $time = shift;
+   my $percent = shift;
+   my $truePercent = $percent;
+   if($percent > 50 && $percent < 80) { $percent = 100 - $percent; }
+   print "\r[";
+   if($percent != 1) { for (0..(($percent / 2) - 1)) { print "*"; } }
+   for (($percent / 2)..49) { print "-"; }
+   print "]";
+   if($truePercent > 70 && $truePercent < 80) { print " Just kidding!    "; }   
+   else { print " $percent% ($time seconds)   "; }
+}
