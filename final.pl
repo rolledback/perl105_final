5cca35ac9ba2802967f588a9a6c29315aa36f94e
rolledback <matthew.rayermann@gmail.com>
2013-10-28 00:55:58 -0500

    regex should be 100% correct now, fixed many bugs with it, also, it's super fast

--- final.pl
+++ final.pl
@@ -3,46 +3,55 @@
 use Data::Dumper;
 use POSIX 'ceil';
 
-%actors;
-$movies;
-$totalActors = 123524;
-$startTime = time();
+sub percentagePrint {
+   $time = shift;
+   $percent = shift;
+   print "\r";
+   for $i (0..($percent / 2)) { print "*"; }
+   print " $percent% ($time seconds)"; 
+}
 
-print "\nProgress:\n";
+my %actors;
+my $movies;
+my $totalTime;
+my $actor;
 
 foreach $arg (@ARGV) {
-   @lines = `zcat $arg`;
-   $percent = 0;
+   print "Opening $arg...\n";
+   my @lines = `zcat $arg`;
+   my $startTime = time();
+   print "Done.\nParsing $arg...\n";
+   $totalLines = scalar @lines;
+   my $line = 0;
    foreach (@lines) {
+      $line++;
+      if($line % 100 == 0) {
+         percentagePrint(time() - $startTime, ceil($line / $totalLines * 100));
+      }
       chomp();
-      if($_ =~ /([\w\d \S]*)(\t+)(.+)(\([\d]+(\/[IXV]+)*\))(.*)/) {   
-         if($1) { 
-            $actor = $1;
-            $numDone = keys %actors;
-            if($numDone % 15 == 0) {
-               $runTime = time() - $startTime;
-               $percent = ceil(($numDone / $totalActors) * 10);
-               print "\r";
-               for $i (0..($percent / 2)) { print "*"; }
-               print " $percent% ($runTime seconds)";       
-            }
+      if($_ =~ /^(.*)\t+(.+?\([\d]+(\/[IXV]*)*\))(.*)/) {  
+         #print ("\$1: $1\t\$2: $2\t\$3: $3\t\$4: $4\n");
+         (my $gActor, my $gMovie, my $gRoman, my $gExtra) = ($1, $2, $3, $4);
+         if($gActor !~ /^\s*$/) {
+            $actor = $gActor;      
          }
-         if(index($6, "(TV)") == -1 && index($6, "(VG)") == -1 && index($6, "(V)") == -1) {
-            $name = "$3$4";
-            if($name !~ /".+"/) {
-               push(@{$actors{$actor}}, $name);
-               push(@{$movies{$name}}, $actor);
+         if(index($gExtra, "(TV)") == -1 && index($gExtra, "(VG)") == -1 && index($gExtra, "(V)") == -1 && index($gExtra, "(archive footage)") == -1) {
+            if(index($gMovie, "\"") != 0) {
+               push(@{$actors{$actor}}, $gMovie);
+               push(@{$movies{$gMovie}}, $actor);
+		         #print "$actor\n\t$gMovie\n";
             }
          }
       }
    }
+   $totalTime += (time() - $startTime);
+   print "\nDone.\n\n";
 }
-print "\n";
 
-$numActors = keys %actors;
+$numActors = keys %actors; 
 $numMovies = keys %movies;
-$rate = ceil($numActors / $runTime);
-print "\nA total of $numActors actors in $numMovies movies parsed in $runTime seconds, at a rate of $rate actors/second.\n";
+$rate = ceil($numActors / $totalTime);
+print "\nA total of $numActors actors in $numMovies movies parsed in $totalTime seconds, at a rate of $rate actors/second.\n";
  
 #print Dumper \%actors;
-#print Dumper \%movies;clear
+#print Dumper \%movies;
